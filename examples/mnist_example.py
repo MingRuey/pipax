2ff04dfbb5f2fc8059fd3a662fcd8c33d3795301
mrchou <imchou239@gmail.com>
2021-11-26 19:23:12 -0500

    Draft the benchmark

---
+++ examples/mnist_example.py
@@ -0,0 +1,11 @@
+"""
+A simple example for training MLP on MNIST (without model/pipeline parallelism)
+"""
+
+import pytest
+
+class TestMlpOnMnist:
+    pass
+
+if __name__ == "__main__":
+    pytest.main(["-s", "-v", __file__])
---
+++ examples/pipeline_example.py
@@ -0,0 +1,27 @@
+"""
+A simple example for benchmark the performance of pipelined MLP on MNIST
+"""
+
+import pytest
+
+class TestBenchmarkSingleDevice:
+    pass
+
+class TestBenchmarkCrossDevice:
+
+    def test_simple_model_parallelism(self):
+        """Split the MLP across devices, and use naive scheduling, i.e. without Pipeline parallelism"""
+        pass
+
+    def test_gpipe_scheduling(self):
+        """Split the MLP across devices, and use GPipe scheduling
+
+        Reference:
+            GPipe: Efficient Training of Giant Neural Networks using Pipeline Parallelism
+            https://arxiv.org/abs/1811.06965
+        """
+        pass
+
+
+if __name__ == "__main__":
+    pass
